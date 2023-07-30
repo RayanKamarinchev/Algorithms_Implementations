2c7f859faa91f924b018884417f26f1a95586bc7
RayanKamarinchev <rayan2008bg@gmail.com>
2023-07-30 11:44:09 +0300

    binary heap simple implementation

---
+++ BinaryHeapProj/BinaryHeap.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinaryHeapProj
+{
+    public class BinaryHeap<T> where T : IComparable<T>
+    {
+        private List<T> heapList;
+        private int size;
+        public BinaryHeap()
+        {
+            heapList = new List<T>();
+        }
+
+        private void Swap(T item1, T item2)
+        {
+            (item1, item2) = (item2, item1);
+        }
+
+        private int GetParent(int i)
+        {
+            return (i - 1) / 2;
+        }
+
+        private int GetLeft(int i)
+        {
+            return i*2 + 1;
+        }
+
+        private int GetRight(int i)
+        {
+            return i * 2 + 2; 
+        }
+
+        public void Add(T item)
+        {
+            heapList.Add(item);
+            size++;
+
+            int i = size - 1;
+            
+            while (i !=0 && heapList[i].CompareTo(heapList[GetParent(i)]) > 0)
+            {
+                Swap(heapList[i], heapList[GetParent(i)]);
+                i = GetParent(i);
+            }
+        }
+
+        private int IndexOf(T item)
+        {
+            int i = 0;
+            T currentItem = heapList[i];
+            while (!currentItem.Equals(item))
+            {
+                if (item.CompareTo(currentItem) < 0)
+                {
+                    i = GetLeft(i);
+                }
+                else
+                {
+                    i = GetRight(i);
+                }
+
+                if (i >= size)
+                {
+                    return -1;
+                }
+                currentItem = heapList[i];
+            }
+
+            return i;
+        }
+
+        public void Remove(T item)
+        {
+            int i = IndexOf(item);
+            if (i == -1)
+            {
+                return;
+            }
+            Swap(heapList[i], heapList[size-1]);
+
+            while (i != 0 && heapList[i].CompareTo(heapList[GetParent(i)]) < 0)
+            {
+                Swap(heapList[i], heapList[GetParent(i)]);
+                i = GetParent(i);
+            }
+        }
+    }
+}
---
+++ BinaryHeapProj/Program.cs
@@ -0,0 +1,11 @@
+﻿using BinaryHeapProj;
+
+BinaryHeap<int> h = new BinaryHeap<int>();
+h.Add(3);
+h.Add(2);
+h.Remove(1);
+h.Add(15);
+h.Add(5);
+h.Add(4);
+h.Add(45);
+Console.WriteLine(h);
