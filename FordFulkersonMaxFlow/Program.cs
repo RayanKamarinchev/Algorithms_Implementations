3b439c44b69a3ffd3cda816a23234804319f3308
RayanKamarinchev <rayan2008bg@gmail.com>
2023-07-31 19:40:01 +0300

    MaxFlow algorithms

---
+++ FordFulkersonMaxFlow/Program.cs
@@ -0,0 +1,79 @@
+﻿int[,] graph = new int[,] {
+    { 0, 16, 13, 0, 0, 0 },
+    { 0, 0, 10, 12, 0, 0 },
+    { 0, 4, 0, 0, 14, 0 },
+    { 0, 0, 9, 0, 0, 20 },
+    { 0, 0, 0, 7, 0, 4 },
+    { 0, 0, 0, 0, 0, 0 }
+};
+int nodes = 6;
+
+Console.WriteLine(findMaxFlow(graph, 0, 5));
+
+int findMaxFlow(int[,] graph, int source, int sink)
+{
+    //create graph with capacities
+    int[,] rGraph = new int[nodes, nodes];
+    for (int i = 0; i < nodes; i++)
+    {
+        for (int j = 0; j < nodes; j++)
+        {
+            rGraph[i, j] = graph[i, j];
+        }
+    }
+
+    //Store path
+    int[] parent = new int[nodes];
+    int maxFlow = 0;
+
+    while (Bfs(rGraph, source, sink, parent))
+    {
+        //backwards, find the bottleneck (the lowest value)
+        int pathFlow = int.MaxValue;
+        for (int i = sink; i != source; i = parent[i])
+        {
+            int j = parent[i];
+            pathFlow = Math.Min(pathFlow, rGraph[j, i]);
+        }
+
+        for (int i = sink; i != source; i = parent[i])
+        {
+            int j = parent[i];
+            rGraph[j, i] -= pathFlow;
+            rGraph[i, j] += pathFlow;
+        }
+
+        maxFlow += pathFlow;
+    }
+    return maxFlow;
+}
+
+bool Bfs(int[,] graph, int source, int sink, int[] parent)
+{
+    bool[] visited = new bool[nodes];
+    Queue<int> queue = new Queue<int>();
+    queue.Enqueue(source);
+    visited[source] = true;
+    parent[source] = -1;
+
+    while (queue.Count > 0)
+    {
+        int current = queue.Dequeue();
+        for (int j = 0; j < nodes; j++)
+        {
+            if (graph[current, j] > 0 && !visited[j])
+            {
+                if (j == sink)
+                {
+                    parent[j] = current;
+                    return true;
+                }
+                queue.Enqueue(j);
+                parent[j] = current;
+                visited[j] = true;
+            }
+        }
+    }
+
+    return false;
+}
---
+++ MaxFlowCapacityScaling/FlowGraph.cs
@@ -0,0 +1,97 @@
+﻿namespace DinicsAlgorithm
+{
+    public class FlowGraph
+    {
+        private int[,] graph;
+        private int[,] flow;
+        private int[] level;
+        private int[] parent;
+        private int nodes;
+
+        public FlowGraph(int nodes)
+        {
+            this.nodes = nodes;
+            graph = new int[nodes, nodes];
+            level = new int[nodes];
+            parent = new int[nodes];
+        }
+
+        public void AddEdge(int from, int to, int cap)
+        {
+            graph[from, to] += cap;
+        }
+
+        private bool BFS(int source, int sink)
+        {
+            Array.Fill(level, -1);
+            level[source] = 0;
+
+            Queue<int> queue = new Queue<int>();
+            queue.Enqueue(source);
+
+            while (queue.Count != 0)
+            {
+                int v = queue.Dequeue();
+                for (int i = 0; i < nodes; i++)
+                {
+                    //checks if unvisited and if flow can go through
+                    if (level[i] < 0 && 
+                        flow[v, i] < graph[v, i])
+                    {
+                        queue.Enqueue(i);
+                        level[i] = level[v] + 1;
+                    }
+                }
+            }
+
+            return level[sink] >= 0;
+        }
+
+        private int DFS(int current, int flowTillNow, int sink)
+        {
+            if (current == sink)
+                return flowTillNow;
+
+            while (parent[current] < nodes)
+            {
+                int i = parent[current]++;
+                if (level[i] == level[current] + 1 && flow[current, i] < graph[current, i])
+                {
+                    int curr_flow = Math.Min(flowTillNow, graph[current, i] - flow[current, i]);
+                    int temp_flow = DFS(i, curr_flow, sink);
+
+                    if (temp_flow > 0)
+                    {
+                        flow[current, i] += temp_flow;
+                        flow[i, current] -= temp_flow;
+                        return temp_flow;
+                    }
+                }
+            }
+
+            return 0;
+        }
+
+        public int MaxFlow(int source, int sink)
+        {
+            if (source == sink)
+                return -1;
+
+            int total = 0;
+
+            while (BFS(source, sink))
+            {
+                Array.Fill(parent, 0);
+                while (true)
+                {
+                    int flow = DFS(source, int.MaxValue, sink);
+                    if (flow == 0)
+                        break;
+                    total += flow;
+                }
+            }
+
+            return total;
+        }
+    }
+}
